233b02c6ed9360eb28174733898e3d060626baaf
fperez <PEREZFAQ98-2020@ies21.onmicrosoft.com>
2023-03-08 10:26:39 -0300

    sync

--- SistemaTesis.IOC/Dependencia.cs
+++ SistemaTesis.IOC/Dependencia.cs
@@ -8,12 +8,24 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SistemaTesis.DAL.DBContext;
 using Microsoft.EntityFrameworkCore;
-using SistemaTesis.DAL.Interfaces;
+using System.Runtime.CompilerServices;
+//using SistemaTesis.DAL.Interfaces;
+//using SistemaTesis.DAL.Implementacion;
+//using SistemaTesis.BLL.Interfaces;
+//using SistemaTesis.BLL.Implementacion;
 
 namespace SistemaTesis.IOC
 {
     public static class Dependencia
     {
-
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<Tesisv1Context>(options =>
+            {
+                options.UseMySql("CadenaSQL", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+            });
+        }
     }
+
+
 }
